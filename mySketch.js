9573edce61179c336b3adb13ed99cb4fa35257fc
NICOLASSTEVEN <47667767+NICOLASSTEVEN@users.noreply.github.com>
2019-02-15 10:48:02 -0500

    Add files via upload

---
+++ mySketch.js
@@ -0,0 +1,38 @@
+
+this.xpos=0;
+this.ypos=0;
+this.vel=3;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(100);
+
+}
+
+function draw() {
+	
+	noStroke();
+	
+	this.xpos = this.xpos + this.vel;
+	if(this.xpos > width){
+	 this.xpos = -50;
+	}	
+	
+	this.ypos = this.ypos - this.vel;
+	if(this.ypos < 1 ){
+	 this.ypos = 1900;
+	}	
+	
+	fill(91, 44, 111,50);
+	ellipse(this.xpos,random(2000),random(200),random(200));
+	
+	fill(123, 36, 28,50);
+	ellipse(random(2000),this.xpos,random(200),random(200));
+	
+		fill(93, 109, 126,50);
+	ellipse(random(2000),this.ypos,random(200),random(200));
+	
+	fill(189, 195, 199 ,50);
+	ellipse(this.ypos,random(2000),random(200),random(200));
+	
+}
