b3f8bd68d3f9b7681b6597cc031c9067a2389cbb
NicolasSteven <47667767+NicolasSteven@users.noreply.github.com>
2019-03-07 13:28:56 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -2,10 +2,14 @@
 this.xpos=0;
 this.ypos=0;
 this.vel=3;
+var canvas;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(100);
+	canvas=createCanvas(windowWidth, windowHeight);
+	canvas.position(0,0);
+	canvas.style('z-index',-1);
+  
+	background(50);
 
 }
 
