6f7b9481d305b1c4e2f7edd29ff582906e2c29fe
NicolasSteven <47667767+NicolasSteven@users.noreply.github.com>
2019-03-09 20:58:18 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -14,9 +14,12 @@ var MAX_RG = 255;
 this.xpos=0;
 this.ypos=0;
 this.vel=3;
+var canvas;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	canvas=createCanvas(windowWidth, windowHeight);
+	canvas.position(0,0);
+	canvas.style('z-index',-1);
 	
 	colorMode(RGB, 250, 305, 255, 500);
 	ellipseMode(RADIUS);
